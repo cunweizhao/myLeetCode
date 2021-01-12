2ac3fff13f7608f4b32f72d4d056fceff5399b22
cunweizhao <18500103508@163.com>
2021-01-12 18:01:56 +0800

    Create Test2.java
    TODO 两数相加

---
+++ src/main/java/com/zcw/leetcode/Test2.java
@@ -0,0 +1,22 @@
+package com.zcw.leetcode;
+
+/**
+ * @program: my-leetcode
+ * @description: 两数相加
+ * @author: Zhaocunwei
+ * @create: 2021-01-12 16:42
+ **/
+
+public class Test2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            return null;
+    }
+}
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
