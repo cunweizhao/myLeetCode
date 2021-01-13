c89a10f71187fdbc6205aacd1d9d2260cb9993d7
cunweizhao <18500103508@163.com>
2021-01-13 14:50:01 +0800

    Update Test2.java
    两数相加

--- src/main/java/com/zcw/leetcode/Test2.java
+++ src/main/java/com/zcw/leetcode/Test2.java
@@ -9,7 +9,38 @@ package com.zcw.leetcode;
 
 public class Test2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            return null;
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        ListNode newHead = new ListNode(0);
+        ListNode head3 = newHead;
+        // 进位标志
+        boolean carry = false;
+        while (head1 != null || head2 != null) {
+            // 获取对应位置的值然后相加
+            int x = (head1 != null) ? head1.val : 0;
+            int y = (head2 != null) ? head2.val : 0;
+            int sum = carry ? (x + y + 1) : (x + y);
+            // 处理进位
+            if (sum >= 10){
+                sum -= 10;
+                carry = true;
+            } else {
+                carry = false;
+            }
+            // 新增节点
+            head3.next = new ListNode(sum % 10);
+            head3 = head3.next;
+            if (head1 != null){
+                head1 = head1.next;
+            }
+            if (head2 != null){
+                head2 = head2.next;
+            }
+        }
+        if (carry) {
+            head3.next = new ListNode(1);
+        }
+        return newHead.next;
     }
 }
 
